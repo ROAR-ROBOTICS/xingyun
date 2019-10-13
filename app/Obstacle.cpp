4a35bcf42d767161da8ebd9b79279eca083ff602
ZuyangCao <nuclearczy@hotmail.com>
2019-10-13 17:16:43 -0400

    Created stub implementation for Obstacle class Corrected misspelling in header file.

--- app/Obstacle.cpp
+++ app/Obstacle.cpp
@@ -1,8 +1,8 @@
 /**Copyright (c) 2019 Hao Da (Kevin) Dong, Zuyang Cao, Jing Liang
- * @file       Obstacle.hpp
+ * @file       Obstacle.cpp
  * @date       10/13/2019
- * @brief      The file Obstacle.hpp contains the header declarations for a
- *             obstacle class. The class will be used in Xingyun class.
+ * @brief      The file Obstacle.cpp implements the obstacle class.
+ *             The class will be used in Xingyun class.
  * @license    This project is released under the BSD-3-Clause License.
  */
 #pragma once
