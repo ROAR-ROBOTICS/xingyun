4c8b39502bea0f761fb2cdc71743b5302e8f647d
ZuyangCao <nuclearczy@hotmail.com>
2019-10-19 01:52:38 -0400

    [main.cpp][Task 2.6] Adding including files in main.cpp file Corrected including files in main file.

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include "Obstacle.hpp"
-#include "Human.hpp"
-#include "Xingyun.hpp"
-
-int main()
-{
-    return 0;
+#include <Obstacle.hpp>
+#include <Human.hpp>
+#include <Xingyun.hpp>
+#include "Xingyun.cpp"
+int main() {
+  return 0;
 }
